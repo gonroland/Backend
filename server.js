47cf14f376c8fc6a0a66a18199f2942fc3b7e30a
Gonzalo <gonzalo.d.rolando@gmail.com>
2024-01-02 14:19:44 -0300

    changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = socketIO(server);
 const PORT = process.env.PORT || 8080;
 
 // Configuración de la conexión a MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/coderhouse', {
+mongoose.connect('mongodb+srv://gonzalodrolando:<password>@coderhouse.opzbd1v.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
